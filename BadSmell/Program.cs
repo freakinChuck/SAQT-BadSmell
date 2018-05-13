917a4018e19e4ca79e4fa703b631a5f72fc3020c
Silvio <silvio.stappung@hotmail.com>
2018-05-13 11:29:11 +0200

    Initial Solution Commit

---
+++ BadSmell/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BadSmell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
